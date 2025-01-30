893825c929a5b47ba78d07a02de10e77485433bc
OwenPhilliber1 <owenphilliber@ucsb.edu>
2025-01-30 14:55:55 -0800

    Added fossil fuel jobs to the data set and exported the data frames to csv into the Data folder

--- green_job_skills.R
+++ green_job_skills.R
@@ -2,6 +2,7 @@ install.packages("readxl")
 
 library("readxl")
 library("dplyr")
+library("tidyverse")
 
 # Data was collected from: https://www.onetcenter.org/dictionary/29.1/excel/skills.html
 
@@ -11,38 +12,62 @@ my_data <- read_excel("./data/Skills.xlsx")
 data_df <- data.frame(my_data)
 
 green_jobs <- list('47-2231', '49-9081', '49-9099', '47-4099', '47-1011', '41-4011', '47-2211', '49-9042', '51-9012', '51-8099', '51-8013', '51-8012', '51-8011', '51-4041', '19-4041', '19-4051', '17-2051', '17-2071', '17-2141', '17-2199', '11-3051', '11-3071', '11-9041', '11-9199')
+ff_jobs <- list('47-5011', '47-5012', '47-5013', '47-5022', '47-5041', '47-5043', '47-5044', '47-5071', '47-5081')
 
 data_df <- data_df %>% 
   mutate(short_code = substr(O.NET.SOC.Code, 1, 7))
 
 skills_df <- data_df[,c('O.NET.SOC.Code', 'short_code', 'Title', 'Element.Name', 'Scale.ID', 'Data.Value')]
 
-green_skills_df <- skills_df[skills_df$short_code %in% green_jobs, ]
 
+# Cleaning up green data
+skills_green <- skills_df[skills_df$short_code %in% green_jobs, ]
 
-job_titles <- green_skills_df$Title
-job_titles <- job_titles[!duplicated(job_titles)]
-job_titles
+job_titles_green <- skills_green$Title
+job_titles_green <- job_titles_green[!duplicated(job_titles_green)]
+job_titles_green
 
-job_skills <- green_skills_df$Element.Name
-job_skills <- job_skills[!duplicated(job_skills)]
-job_skills
+job_skills_green <- skills_green$Element.Name
+job_skills_green <- job_skills_green[!duplicated(job_skills_green)]
+job_skills_green
 
-green_skills_df_lvl <- green_skills_df[green_skills_df$Scale.ID == 'LV', ]
-green_skills_df_imp <- green_skills_df[green_skills_df$Scale.ID == 'IM', ]
+skills_green_lvl <- skills_green[skills_green$Scale.ID == 'LV', ]
+skills_green_imp <- skills_green[skills_green$Scale.ID == 'IM', ]
 
-green_skills_df_lvl_wide <- green_skills_df_lvl %>%
+skills_green_lvl_wide <- skills_green_lvl %>%
   pivot_wider(names_from = Element.Name, values_from = Data.Value)
 
-green_skills_df_imp_wide <- green_skills_df_imp %>%
+skills_green_imp_wide <- skills_green_lvl %>%
+  pivot_wider(names_from = Element.Name, values_from = Data.Value)
+
+# Cleaning up ff data
+skills_ff <- skills_df[skills_df$short_code %in% ff_jobs, ]
+
+job_titles_ff <- skills_ff$Title
+job_titles_ff <- job_titles_ff[!duplicated(job_titles_ff)]
+job_titles_ff
+
+job_skills_ff <- skills_ff$Element.Name
+job_skills_ff <- job_skills_ff[!duplicated(job_skills_ff)]
+job_skills_ff
+
+skills_ff_lvl <- skills_ff[skills_ff$Scale.ID == 'LV', ]
+skills_ff_imp <- skills_ff[skills_ff$Scale.ID == 'IM', ]
+
+skills_ff_lvl_wide <- skills_ff_lvl %>%
+  pivot_wider(names_from = Element.Name, values_from = Data.Value)
+
+skills_ff_imp_wide <- skills_ff_imp %>%
   pivot_wider(names_from = Element.Name, values_from = Data.Value)
 
 
-green_skills_lvl_value <- green_skills_df[green_skills_df$Scale.ID == 'LV', "Data.Value"]
-green_skills_imp_value <- green_skills_df[green_skills_df$Scale.ID == 'IM', "Data.Value"]
+# Analyzing level vs importance
+green_skills_lvl_value <- skills_green[skills_green$Scale.ID == 'LV', "Data.Value"]
+green_skills_imp_value <- skills_green[skills_green$Scale.ID == 'IM', "Data.Value"]
 
 graph_data <- cbind(green_skills_lvl_value, green_skills_imp_value)
 
+# Plot shows that they are highly correlated
 ggplot(data = graph_data, aes(x = green_skills_lvl_value, y = green_skills_imp_value)) +
   geom_point(alpha = 0.3, color = "darkseagreen4") +
   geom_smooth(method = "lm", color = "darkseagreen3") +
@@ -50,5 +75,12 @@ ggplot(data = graph_data, aes(x = green_skills_lvl_value, y = green_skills_imp_v
   labs(title = "Ploting the Level of a Skill to the Importance of a Skill", x = "Level", y = "Importance") +
   theme(panel.background = element_rect(fill = "#F5FFF7"),
         plot.background = element_rect(fill = "white"))
+
+
+# Exporting wide data frames
+setwd('./Data')
+write.csv(skills_green_lvl_wide,"./skills_green_lvl.csv", row.names = FALSE)
+write.csv(skills_ff_lvl_wide,"./skills_ff_lvl.csv", row.names = FALSE)
+
 
 
