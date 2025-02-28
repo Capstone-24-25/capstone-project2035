d29a4ced46a6aa6c252706b2cee8b70480b4f4d3
Mai Huynh <mthuynh@umail.ucsb.edu>
2025-02-28 15:14:43 -0800

    Add graph aggregating wage data by county and by job type
    used median wage of counties

---
+++ scripts/wage_by_year.R
@@ -0,0 +1,17 @@
+library(ggplot2)
+library(tidyverse)
+
+data <- read.csv("../Data/median_yearly_earning.csv")
+  
+data %>% 
+  mutate(estimate = as.numeric(estimate)) %>% 
+  filter(job_type != 'Other') %>% 
+  group_by(county,year, job_type) %>% 
+  summarise(med_wage = median(estimate, na.rm = T)) %>% 
+  ggplot(aes(x = year, y = med_wage,  color = job_type)) +
+  geom_line() +
+  labs(title = "Median Yearly Wages by County and Job Type",
+       x = "Year",
+       y = "Median Wage",
+       color = "Job Type")+
+  facet_wrap(~county)
